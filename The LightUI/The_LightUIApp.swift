a523b5aebe7b89cc35901144dd791daf0adc9d20
Konstantin Ryabtsev <kryabtsev83@gmail.com>
2021-04-14 02:12:23 +0300

    Initial Commit

---
+++ The LightUI/The_LightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_LightUIApp.swift
+//  The LightUI
+//
+//  Created by Konstantin on 14.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct The_LightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
